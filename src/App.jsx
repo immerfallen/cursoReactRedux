a8900dca9ff86423dbbbf743e3e1ab803e7da583
Maro de Melo <marodemelo10@gmail.com>
2021-07-31 12:33:45 -0300

    passando propriedades do componente pai para o componente filho

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,29 @@ import ComParam from "./Components/basicos/ComParametro";
 import Fragmento from "./Components/basicos/Fragamento";
 import Aleatorio from "./Components/basicos/Aleatorio";
 import Card from "./Components/layout/Card";
+import Familia from "./Components/basicos/Familia";
 
 export default () => (
   <div className="App">
     <h1>Fundamento de React</h1>
 
     <div className="Cards">
-      <Card titulo="Desafio aleatório" color="#2e003e">
+      <Card titulo="# 04 - Desafio aleatório" color="#2e003e">
         <Aleatorio minimo={10} maximo={20} />
       </Card>
-      <Card titulo="Primeiro Componente" color="#3d2352">
+      <Card titulo="#05 - Componente com filhos" color="#C956F7">
+        <Familia sobrenome="Justino"/>
+      </Card>
+      <Card titulo=" #01 - Primeiro Componente" color="#3d2352">
         <Primeiro />
       </Card>
-      <Card titulo="Com parâmetro" color="#3d1e6d">
+      <Card titulo="#02 - Com parâmetro" color="#3d1e6d">
         <ComParam titulo="Situação do aluno" aluno="Maro" nota={9.3} />
       </Card>
-      <Card titulo="Com parâmetro 2" color="#8874a3">
+      <Card titulo="# 02 - Com parâmetro 2" color="#8874a3">
         <ComParam titulo="Situação do aluno" aluno="Maria" nota={6.3} />
       </Card>
-      <Card titulo="Fragmento" color="#e4dcf1">
+      <Card titulo=" # 03 - Fragmento" color="#e4dcf1">
         <Fragmento />
       </Card>
     </div>
