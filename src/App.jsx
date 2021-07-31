2701f5ef92120fbde2a649fce9e0a9b6bda062f2
Maro de Melo <marodemelo10@gmail.com>
2021-07-31 12:18:02 -0300

    passando componente como filho de outro componente e estilizando via props

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,19 @@ export default () => (
     <h1>Fundamento de React</h1>
 
     <div className="Cards">
-      <Card titulo="Desafio aleatório">
+      <Card titulo="Desafio aleatório" color="#2e003e">
         <Aleatorio minimo={10} maximo={20} />
       </Card>
-      <Card titulo="Primeiro Componente">
+      <Card titulo="Primeiro Componente" color="#3d2352">
         <Primeiro />
       </Card>
-      <Card titulo="Com parâmetro">
+      <Card titulo="Com parâmetro" color="#3d1e6d">
         <ComParam titulo="Situação do aluno" aluno="Maro" nota={9.3} />
       </Card>
-      <Card titulo="Com parâmetro 2">
+      <Card titulo="Com parâmetro 2" color="#8874a3">
         <ComParam titulo="Situação do aluno" aluno="Maria" nota={6.3} />
       </Card>
-      <Card titulo="Fragmento">
+      <Card titulo="Fragmento" color="#e4dcf1">
         <Fragmento />
       </Card>
     </div>
