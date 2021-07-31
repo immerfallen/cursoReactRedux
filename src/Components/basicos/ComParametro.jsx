3964558268cae79bc8bd9ba66bcd1e45c9f9a2ac
Maro de Melo <marodemelo10@gmail.com>
2021-07-31 10:10:58 -0300

    criando primeiro component com props

---
+++ src/Components/basicos/ComParametro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ComParametro(props){
+    const status = props.nota >= 7 ? 'Aprovado' : 'Reprovado'
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <p> <strong> {props.aluno} </strong> tem nota {props.nota}</p>   
+            <p>Aluno {status} </p>        
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDom from 'react-dom'
 import React from 'react'
 import './index.css'
 import Primeiro from './Components/basicos/Primeiro'
+import ComParam from './Components/basicos/ComParametro'
 
 const el = document.getElementById('root')
 
@@ -9,6 +10,8 @@ const el = document.getElementById('root')
 ReactDom.render( 
     <div>
         <Primeiro/>
+        <ComParam titulo='Situação do aluno' aluno='Maro' nota={9.3}/>
+        <ComParam titulo='Situação do aluno' aluno='Maria' nota={6.3} status/>
     </div>, 
     el 
     )
